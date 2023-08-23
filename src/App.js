1161bef0b8f4a10a351d51100098e935a3788024
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-08-23 09:51:07 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+
+function App() {
+  // render(){
+  return (
+    <DynamicTable />
+  );
+}
+
+
+export default App;
